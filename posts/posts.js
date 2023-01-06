d1e29c7652323dfc3ca32974bdf6a183783b5b4d
Megan Sampson <msampson@nyc.yearup.org>
2023-01-05 21:41:57 -0500

    post page

--- posts/posts.js
+++ posts/posts.js
@@ -3,9 +3,9 @@
 "use strict";
 
 window.onload = displayUserPost() 
-const signOutBtn = document.getElementById("signOutBtn")
+const signoutBtn = document.getElementById("signoutBtn")
 
-signOutBtn.onclick = function () {
+signoutBtn.onclick = function () {
     logout();
 }
 
@@ -23,22 +23,43 @@ function displayUserPost() {
     .then(response => response.json())
     .then(data =>   {
         for(let i=0; i<data.length; i++) {
+            let postOutput = document.createElement("div")
             let postUsername = document.createElement("div")
             let postContent = document.createElement("div")
             let postCreation = document.createElement("div")
             
+            postUsername.classList.add('postUserName')
+            postContent.classList.add('postContent')
+            postCreation.classList.add('postCreation')  
+            postOutput.classList.add('postOutput')  
+
+            postOutput.appendChild(postUsername);
+            postOutput.appendChild(postContent);
+            postOutput.appendChild(postCreation);
             
     
           postUsername.innerHTML = `<h7 <span class='fw-bold'> ${data[i].username} </span></h7> `
           postContent.innerHTML = `<h8 <span class='fw-bold'> <br>  ${data[i].text}  <br> </span></h8> `
-          postCreation.innerHTML = `<small> ${data[i].createdAt} <small>`
+          postCreation.innerHTML = `<small> ${prettyDate(data[i].createdAt)} <small>`
           
-    
-          postOutputList.appendChild(postUsername);
-          postOutputList.appendChild(postContent);
-          postOutputList.appendChild(postCreation);
+        postOutputList.appendChild(postOutput);
           
         }
     })
     
 }
+
+function prettyDate (date) {
+    let givenDate = new Date(date);
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = givenDate.getDate();
+    let month = months[givenDate.getMonth()];
+    let year = givenDate.getFullYear();
+    let dayOfWeek = days[givenDate.getDay()];
+    let prettyDate = `${dayOfWeek}, ${month} ${day}, ${year}`
+
+    return prettyDate;
+}
+
